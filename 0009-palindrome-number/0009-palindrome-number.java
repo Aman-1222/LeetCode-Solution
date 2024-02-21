9746bbd333d4ee93fbb3e6d8adb956890f60f7d7
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-02-21 19:16:43 +0530

    Time: 6 ms (43.96%), Space: 44 MB (44.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String digit=String.valueOf(x);
+        int len=digit.length()-1;
+        int i=0;
+        while(i<=len){
+            if(digit.charAt(i)!=digit.charAt(len))  return false;
+            i++;
+            len--;
+            }
+        return true;
+    }
+}
