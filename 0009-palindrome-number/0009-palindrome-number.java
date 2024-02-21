4a7d24e3938437fc7fc7deb106f4f79ad9d310db
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-02-21 19:26:22 +0530

    Time: 4 ms (100.00%), Space: 43.6 MB (95.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,22 +1,24 @@
-class Solution {
-    public boolean isPalindrome(int x) {
-        String digit=""+x;
-        int len=digit.length()-1;
-        int i=0;
-        while(i<=len){
-            if(digit.charAt(i)!=digit.charAt(len))  return false;
-            i++;
-            len--;
-            }
-        return true;
-    }
-}
-// //public boolean isPalindrome(int x) {
-//     if (x<0 || (x!=0 && x%10==0)) return false;
-//     int rev = 0;
-//     while (x>rev){
-//     	rev = rev*10 + x%10;
-//     	x = x/10;
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         String digit=""+x;
+//         int len=digit.length()-1;
+//         int i=0;
+//         while(i<=len){
+//             if(digit.charAt(i)!=digit.charAt(len))  return false;
+//             i++;
+//             len--;
+//             }
+//         return true;
 //     }
-//     return (x==rev || x==rev/10);
 // }
+class Solution {
+public boolean isPalindrome(int x) {
+    if (x<0 || (x!=0 && x%10==0)) return false;
+    int rev = 0;
+    while (x>rev){
+    	rev = rev*10 + x%10;
+    	x = x/10;
+    }
+    return (x==rev || x==rev/10);
+}
+}
