356a828c788a6550e375ea29d0ba725f80a1758e
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-02-22 15:11:50 +0530

    Time: 2 ms (99.85%), Space: 53.8 MB (43.36%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,23 +1,36 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int len=0;
-        int sum=(n*(n+1))/2;
-        for(int i=0;i<trust.length;i++){
-            if(!map.containsKey(trust[i][0])){
-                len++;
-                map.put(trust[i][0],1);
-                sum-=trust[i][0];
-            }
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // int len=0;
+        // int sum=(n*(n+1))/2;
+        // for(int i=0;i<trust.length;i++){
+        //     if(!map.containsKey(trust[i][0])){
+        //         len++;
+        //         map.put(trust[i][0],1);
+        //         sum-=trust[i][0];
+        //     }
+        // }
+        // if(len==n-1){
+        //     int count=0;
+        //     for(int i=0;i<trust.length;i++){
+        //         if(trust[i][1]==sum) count++;
+        //     }
+        //     if(count==len) return sum;
+        //     return -1;
+        // }
+        //        return -1;
+        
+        
+           
+        int[] count = new int[n+1];
+        for (int[] t: trust) {
+            count[t[0]]--;
+            count[t[1]]++;
         }
-        if(len==n-1){
-            int count=0;
-            for(int i=0;i<trust.length;i++){
-                if(trust[i][1]==sum) count++;
-            }
-            if(count==len) return sum;
-            return -1;
+        for (int i = 1; i <= n; ++i) {
+            if (count[i] == n - 1) return i;
         }
-               return -1;
+        return -1;
     }
+    
 }
