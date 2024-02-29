04a74db21a88148db31f8468551062a0878c3fdc
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-02-29 22:58:35 +0530

    Time: 19 ms (14.95%), Space: 57.4 MB (20.63%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -5,6 +5,7 @@ class Solution {
         if(len<=3) return 0;
         Arrays.sort(nums);
         for(int i=0;i<=len/2;i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
             int h=((i%len)+len-4)%len;
             int d=nums[h]-nums[i];
             if(d>=0)
