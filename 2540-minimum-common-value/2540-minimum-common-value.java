074d8c6c9925fca1dfa160cba903414b62a1da8b
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-03-09 20:46:22 +0530

    Time: 1 ms (96.67%), Space: 62.9 MB (32.96%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,5 +1,6 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
+        if(nums1[nums1.length-1]<nums2[0]) return -1;
         for(int i=0;i<nums1.length;i++){
             if(isCommonInteger(nums2,nums1[i])) return nums1[i];
         }
