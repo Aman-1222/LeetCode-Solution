75a8eb8f555593b09d4525a6784d3065f16500d5
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-03-13 22:16:58 +0530

    Time: 9 ms (17.91%), Space: 44.5 MB (5.03%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,15 +1,15 @@
 class Solution {
     public int pivotInteger(int n) {
-        if (n==1) return 1;
-        for(int i=1;i<=n;i++)
-            if(sum(i,n)) return i;
-        return -1;
-    }
-    
-    public boolean sum(int pivot,int num){
+        HashMap<Integer,Integer> mpp=new HashMap<>();
         int startSum=0, endSum=0;
-        for(int i=1;i<=pivot;i++) startSum+=i;
-        for(int i=pivot;i<=num;i++) endSum+=i;
-        return startSum==endSum;
+        for(int i=1;i<=n;i++){
+            startSum+=i;
+            mpp.put(startSum,i);
+        }
+        for(int i=n;i>=1;i--){
+            endSum+=i;
+            if(mpp.containsKey(endSum) && mpp.get(endSum)==i ) return i;
+        }
+        return -1;
     }
 }
