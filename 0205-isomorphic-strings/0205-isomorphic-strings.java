c5b2bdd022ecde01fb1f7bcaaddd8173bca864db
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-04-02 21:55:33 +0530

    Time: 8 ms (78.67%), Space: 42.3 MB (67.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] arr1=new int[256];
-        int[] arr2=new int[256];
+        int[] arr1=new int[200];
+        int[] arr2=new int[200];
         for(int i=0;i<s.length();i++){
             if(arr1[s.charAt(i)] != arr2[t.charAt(i)]) return false;
             arr1[s.charAt(i)]=i+1;
