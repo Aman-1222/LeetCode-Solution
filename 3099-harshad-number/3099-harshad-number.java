08a241242caf023d79f1bd33134bff1fac83d449
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-04-17 23:43:36 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (23.57%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0;
+        int dup=x;
+        while(dup>0){
+            sum+=dup%10;
+            dup/=10;
+        }
+        if(x%sum==0) return sum;
+        else return -1;
+    }
+}
