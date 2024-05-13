df47bcdf9a47714f4a943819c0d6f71238b4838d
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-05-13 22:25:18 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (44.85%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -2,12 +2,10 @@ class Solution {
     public int matrixScore(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++)
             if(grid[i][0]==0) flipRow(grid[i]);
-        }
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
             if(countZeros(grid,i)) flipColumn(grid,i);
-        }
         int sum=0;
         for(int i=0;i<m;i++){
             int num=n-1;
@@ -19,14 +17,12 @@ class Solution {
         return sum;
     }
     public void flipRow(int arr[]){
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++)
             arr[i] = arr[i]==0? 1 : 0;
-        }
     }
     public void flipColumn(int [][]arr,int n){
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<arr.length; i++)
             arr[i][n]= (arr[i][n]==0)? 1 : 0;
-        }
     }
     public boolean countZeros(int [][]arr, int n){
         int one=0;
