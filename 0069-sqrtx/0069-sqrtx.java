e4534048a8f673903269036f03686e262c82f4b9
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-05-26 22:28:04 +0530

    Time: 1 ms (86.12%), Space: 40.6 MB (61.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        //return (int)Math.sqrt(x);
+        if(x==0 || x==1) return x;
+        int low = 1 , high=x/2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mid==x/mid) return mid;
+            if(mid<x/mid) low=mid+1;
+            else high=mid-1;
+        }
+        return high;
     }
 }
