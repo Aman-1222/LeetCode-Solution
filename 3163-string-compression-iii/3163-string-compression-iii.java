0842df92c25c6a13d2b8b71e0e5eaa1512810068
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-05-27 20:45:56 +0530

    Time: 1009 ms (66.67%), Space: 53.9 MB (33.33%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -4,14 +4,12 @@ class Solution {
         String comp="";
         int cnt=1;
         for(int i=0;i<word.length()-1;i++){
-            if(word.charAt(i+1)==word.charAt(i)){
-                cnt++;
-                if(cnt==9){
-                    comp=comp+cnt+word.charAt(i);
+            if(cnt==9){
+                comp=comp+cnt+word.charAt(i);
                     cnt=1;
-                    i++;
                 }
-            }
+            else if(word.charAt(i+1)==word.charAt(i))
+                cnt++;
             else{
                 comp=comp+cnt+word.charAt(i);
                 cnt=1;
