9060376ddcd1b3445dd9472c7e8729db626941ec
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-05-27 13:14:32 +0530

    Time: 2 ms (47.70%), Space: 41.5 MB (21.41%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,13 +1,16 @@
 class Solution {
     public int specialArray(int[] nums) {
         int max=0;
-        for(int i=0;i<nums.length;i++)
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
             max=Math.max(max,nums[i]);
-        for(int i=0;i<=max;i++){
+            sum+=nums[i];
+        }
+        if(sum==0) return -1;
+        for(int i=1;i<=max;i++){
             int cnt=0;
-            for(int j=0;j<nums.length;j++){
+            for(int j=0;j<nums.length;j++)
                 if(nums[j]>=i) cnt++;
-            }
             if(cnt==i) return cnt;
         }
         return -1;
