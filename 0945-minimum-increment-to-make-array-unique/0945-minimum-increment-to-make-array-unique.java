e2d7c42ca2995bb7cd5227d138c4d2feec7ef468
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-06-15 00:42:53 +0530

    Time: 37 ms (72.13%), Space: 60 MB (51.48%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -3,9 +3,9 @@ class Solution {
         Arrays.sort(nums);
         int cnt=0;
         for(int i=1;i<nums.length;i++){
-            while(nums[i]<=nums[i-1]){
-               cnt++;
-                nums[i]++;
+            if(nums[i]<=nums[i-1]){
+               cnt+=nums[i-1]-nums[i]+1;
+               nums[i]=nums[i-1]+1;
             }
         }
         return cnt;
