b92cccc6bced8c623428db6d6d357d20b6c34dc9
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-07-11 16:00:24 +0530

    Time: 1 ms (99.42%), Space: 42 MB (51.98%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -3,8 +3,7 @@ class Solution {
         int count=0;
         for(int i=0;i<logs.length;i++){
             if(logs[i].charAt(0)=='.' && logs[i].charAt(1)=='.' && count>0) count--;
-            else if(logs[i].charAt(0)=='.') continue;
-            else count++;
+            else if(logs[i].charAt(0)!='.') count++;
         }
         return count;
     }
