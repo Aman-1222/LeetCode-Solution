239879fb09cdc29cea56d7e6cb9ab80cd356555a
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-07-12 11:18:46 +0530

    Time: 1 ms (100.00%), Space: 42 MB (99.84%) - LeetHub

--- 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
@@ -1,8 +1,10 @@
 class Solution {
     public String getEncryptedString(String s, int k) {
-        String str="";
-        for(int i=0;i<s.length();i++)
-            str+=s.charAt((i+k)%s.length());
-        return str;
+        // String str="";
+        // for(int i=0;i<s.length();i++)
+        //     str+=s.charAt((i+k)%s.length());
+        // return str;
+        k=k%s.length();
+        return s.substring(k)+s.substring(0,k);
     }
 }
