eebb43397512040a4256c2b61a5f8d3ea1bd2055
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-07-14 16:55:39 +0530

    Time: 2 ms (100.00%), Space: 43.1 MB (100.00%) - LeetHub

--- 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.java
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.java
@@ -3,7 +3,6 @@ class Solution {
         StringBuffer ans=new StringBuffer();
         for(int i=0;i<s.length();i++){
             if(i<s.length()-1 && s.charAt(i)%2==s.charAt(i+1)%2 && s.charAt(i+1)<s.charAt(i) ){
-                System.out.println(s.charAt(i));
                 ans=ans.append(s.charAt(i+1));
                 ans=ans.append(s.charAt(i));
                 ans=ans.append(s,i+2,s.length());
