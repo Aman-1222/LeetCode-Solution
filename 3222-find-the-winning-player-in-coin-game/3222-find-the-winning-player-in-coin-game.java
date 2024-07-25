11e45eea24d25030aee03d3584c9e60bc8eb0f23
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-07-25 20:43:07 +0530

    Time: 1 ms (22.26%), Space: 41.6 MB (65.41%) - LeetHub

--- 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.java
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.java
@@ -1,8 +1,10 @@
 class Solution {
     public String losingPlayer(int x, int y) {
-       if(x<=y/4 && x%2==0) return "Bob";
-       else if(x<=y/4 && x%2!=0) return "Alice";
-       else if(x>y/4 && (y/4)%2==0) return "Bob";
-       else return "Alice"; 
+       // if(x<=y/4 && x%2==0) return "Bob";
+       // else if(x<=y/4 && x%2!=0) return "Alice";
+       // else if(x>y/4 && (y/4)%2==0) return "Bob";
+       // else return "Alice"; 
+        int min=Math.min(x,y/4);
+        return min%2==0 ? "Bob" : "Alice";
     }
 }
