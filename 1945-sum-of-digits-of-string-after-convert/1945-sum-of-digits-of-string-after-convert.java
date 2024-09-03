8e957791bf914237224adbfb8c204cbeb6240bb4
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-09-03 21:51:53 +0530

    Time: 1 ms (88.60%), Space: 41.5 MB (80.62%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
@@ -1,16 +1,38 @@
 class Solution {
     public int getLucky(String s, int k) {
-        String num="";
+        int num=0;
         int ans=0;
-        for(int i=0;i<s.length();i++)
-            num+=s.charAt(i)-96; 
-        while(k>0){
-            ans=0;
-            for(int i=0;i<num.length();i++)
-                ans+=num.charAt(i)-48;
-            k--;
-            num=""+ans;
+        for(int i=0;i<s.length();i++){
+            num=s.charAt(i)-'a'+1;
+            while(num>0){
+                ans+=num%10;
+                num/=10;
+            }
+        }
+        num=ans;
+    for(int i=1;i<k;i++){
+        ans=0;
+        while(num>0){
+            ans+=num%10;
+            num/=10;
+        }
+        num=ans;
     }
         return ans;
     }
 }
+        
+           
+        
+    //     String num="";
+    //     int ans=0;
+    //     for(int i=0;i<s.length();i++)
+    //         num+=s.charAt(i)-96;  // => s.charAt(i)-'a'+1
+    //     while(k>0){
+    //         ans=0;
+    //         for(int i=0;i<num.length();i++)
+    //             ans+=num.charAt(i)-48;  // num.charAt(i)-'0'
+    //         k--;
+    //         num=""+ans;
+    // }
+    //     return ans;
