85d691b28f9bd4013be7e60e4aa279be60b2dbe2
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-09-22 14:39:27 +0530

    Time: 6 ms (30.28%), Space: 44.5 MB (8.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,24 +1,24 @@
-// class Solution {
-//     public boolean isPalindrome(int x) {
-//         String digit=""+x;
-//         int len=digit.length()-1;
-//         int i=0;
-//         while(i<=len){
-//             if(digit.charAt(i)!=digit.charAt(len))  return false;
-//             i++;
-//             len--;
-//             }
-//         return true;
-//     }
-// }
 class Solution {
-public boolean isPalindrome(int x) {
-    if (x<0 || (x!=0 && x%10==0)) return false;
-    int rev = 0;
-    while (x>rev){
-    	rev = rev*10 + x%10;
-    	x = x/10;
+    public boolean isPalindrome(int x) {
+        String digit=""+x;
+        int len=digit.length()-1;
+        int i=0;
+        while(i<=len){
+            if(digit.charAt(i)!=digit.charAt(len))  return false;
+            i++;
+            len--;
+            }
+        return true;
     }
-    return (x==rev || x==rev/10);  // done for even and odd digits
 }
-}
+// class Solution {
+// public boolean isPalindrome(int x) {
+//     if (x<0 || (x!=0 && x%10==0)) return false;
+//     int rev = 0;
+//     while (x>rev){
+//     	rev = rev*10 + x%10;
+//     	x = x/10;
+//     }
+//     return (x==rev || x==rev/10);  // done for even and odd digits
+// }
+//}
