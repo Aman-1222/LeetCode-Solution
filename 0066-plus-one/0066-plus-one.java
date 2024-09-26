6895cc6490e3fb75f7ca305fe88e7ddcf020b498
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-09-26 17:56:35 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (32.69%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        StringBuilder str = new StringBuilder();
+        int carry=0;
+        if(digits[digits.length-1]+1>9){
+            carry=1;
+            str.append(0);
+        }
+        else str.append(digits[digits.length-1]+1);
+        for(int i=digits.length-2;i>=0;i--){
+            if(digits[i]==9 && carry==1){
+                carry=1;
+                str.append(0);
+            }
+            else{
+                str.append(digits[i]+carry);
+                carry=0;
+            }
+        }
+        if(carry==1) str.append(1);
+        int[] ans=new int[str.length()];
+        for(int i=0;i<ans.length;i++)
+            ans[i]=str.charAt(str.length()-1-i)-'0';
+        return ans;
+    }
+}
