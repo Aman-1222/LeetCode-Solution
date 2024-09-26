336d6d14ffd34a1a1bc00fabc855b07f925ff431
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2024-09-26 18:11:21 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (68.40%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,26 +1,41 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        StringBuilder str = new StringBuilder();
-        int carry=0;
-        if(digits[digits.length-1]+1>9){
-            carry=1;
-            str.append(0);
-        }
-        else str.append(digits[digits.length-1]+1);
-        for(int i=digits.length-2;i>=0;i--){
-            if(digits[i]==9 && carry==1){
-                carry=1;
-                str.append(0);
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
             }
-            else{
-                str.append(digits[i]+carry);
-                carry=0;
-            }
+            digits[i]=0;
         }
-        if(carry==1) str.append(1);
-        int[] ans=new int[str.length()];
-        for(int i=0;i<ans.length;i++)
-            ans[i]=str.charAt(str.length()-1-i)-'0';
+        int ans[]=new int[digits.length+1];
+        ans[0]=1;
         return ans;
     }
 }
+    
+        
+        
+        //         // StringBuilder str = new StringBuilder();
+//         // int carry=0;
+//         // if(digits[digits.length-1]+1>9){
+//         //     carry=1;
+//         //     str.append(0);
+//         // }
+//         // else str.append(digits[digits.length-1]+1);
+//         // for(int i=digits.length-2;i>=0;i--){
+//         //     if(digits[i]==9 && carry==1){
+//         //         carry=1;
+//         //         str.append(0);
+//         //     }
+//         //     else{
+//         //         str.append(digits[i]+carry);
+//         //         carry=0;
+//         //     }
+//         // }
+//         // if(carry==1) str.append(1);
+//         // int[] ans=new int[str.length()];
+//         // for(int i=0;i<ans.length;i++)
+//         //     ans[i]=str.charAt(str.length()-1-i)-'0';
+//         // return ans;
+//     }
+// }
