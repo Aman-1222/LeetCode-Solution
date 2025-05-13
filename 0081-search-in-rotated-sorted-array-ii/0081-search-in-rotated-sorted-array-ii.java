1141cee1a38a314d8168475c1c8e209e22098144
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-05-13 20:28:11 +0530

    Time: 0 ms (100%), Space: 43 MB (91.14%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int len=nums.length;
-        int low=0,high=len-1;
+        int low =0, high= nums.length-1;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]==target) return true;
-            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
-                high--;
-                low++;
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+                low+=1;
+                high-=1;
                 continue;
             }
-            if(nums[mid]<=nums[high]){
-                if(nums[mid]<=target && target<=nums[high]) low=mid+1;
-                else high=mid-1;
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid])
+                    high=mid-1;
+                else low=mid+1;
             }
             else{
-                if(nums[low]<=target && target<=nums[mid]) high=mid-1;
-                else low=mid+1;
+                if(target>nums[mid] && target<=nums[high])
+                   low=mid+1;
+                else high=mid-1;
             }
         }
         return false;
