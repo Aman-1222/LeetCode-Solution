d3c76f23c10e2527dbd4d26840275279777b92d4
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-05-13 21:21:14 +0530

    Time: 0 ms (100%), Space: 45.9 MB (83.7%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,36 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int len=nums.length;
-        if(len==0) return new int[]{-1,-1};
-        int firstPos=firstPosition(nums,len,target);
-        if(firstPos==-1) return new int[]{-1,-1};
-        int lastPos=lastPosition(nums,len,target);
-        return new int[]{firstPos,lastPos};
+        return new int[]{firstOccur(nums,target),lastOccur(nums,target)};
     }
-    public static int firstPosition(int[] nums,int len,int target){
-        int low=0;
-        int high=len-1;
-        int index=-1;
+
+    public int firstOccur(int nums[], int target){
+        int low=0,high=nums.length-1;
+        boolean flag=false;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==target) {
-                index=mid;
+            if(nums[mid]==target){
                 high=mid-1;
-            }
-            else if(nums[mid]<target) low=mid+1;
+                flag=true;
+                
+            } 
+            if(nums[mid]<target) low=mid+1;
             else high=mid-1;
         }
-        return index;
+        return (flag)? low : -1;
     }
-        public static int lastPosition(int[] nums,int len,int target){
-        int low=0;
-        int high=len-1;
-        int index=-1;
+    public int lastOccur(int nums[], int target){
+        int low=0,high=nums.length-1;
+        boolean flag=false;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==target) {
-                index=mid;
+            if(nums[mid]==target){
                 low=mid+1;
+                flag=true;
+                continue;
             }
-            else if(nums[mid]<target) low=mid+1;
+            if(nums[mid]<target) low=mid+1;
             else high=mid-1;
         }
-        return index;
+        return (flag)? high : -1;
     }
 }
