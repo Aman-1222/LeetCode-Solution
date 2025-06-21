b2e8151127647d8c779374ce41deb9fee409e2ed
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-06-21 18:40:25 +0530

    Time: 2 ms (83.14%), Space: 45.8 MB (56.35%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,9 +3,9 @@ class Solution {
         int maxInd=0;
         for(int i=0;i<nums.length;i++){
             if(i>maxInd) return false;
-            if(maxInd>=nums.length-1) return true;
             maxInd=Math.max(maxInd,i+nums[i]);
-        }
+            if(maxInd>=nums.length-1) return true;
+            }
         return true;
     }
 }
