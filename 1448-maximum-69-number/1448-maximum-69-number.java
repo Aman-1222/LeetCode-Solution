8f0d34944a1f9f5f5cf0db9863ee32598c901223
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-08-17 18:55:22 +0530

    Time: 0 ms (100%), Space: 40.4 MB (72.7%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,7 +1,53 @@
 class Solution {
     public int maximum69Number (int num) {
-        String s=""+num;
-        s=s.replaceFirst("6","9");
-        return Integer.valueOf(s);
+        int temp=num;
+        int place=1;
+        int ans=num;
+        while(temp>0){
+            int digit=temp%10;
+            if(digit==6)
+                ans=num+3*place;
+            temp/=10;
+            place*=10;
+        }
+        return ans;
+
+        // String s=""+num;
+        // s=s.replaceFirst("6","9");
+        // return Integer.valueOf(s);
     }
 }
+
+// public class ExtractDigits {
+//     public static void main(String[] args) {
+//         int num = 98765;
+//         String s = String.valueOf(num);
+
+//         for (int i = 0; i < s.length(); i++) {
+//             int digit = s.charAt(i) - '0'; // convert char to int
+//             System.out.println(digit);
+//         }
+//     }
+// }
+
+
+// public class ExtractDigits {
+//     public static void main(String[] args) {
+//         int num = 98765;
+
+//         // Find divisor = 10000 for 98765
+//         int divisor = 1;
+//         while (num / divisor >= 10) {
+//             divisor *= 10;
+//         }
+
+//         // Extract digits from front to end
+//         while (divisor > 0) {
+//             int digit = num / divisor;   // get front digit
+//             System.out.println(digit);
+
+//             num = num % divisor;         // remove that digit
+//             divisor /= 10;               // move to next digit
+//         }
+//     }
+// }
