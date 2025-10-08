16105644f7726b147d2900510f8bb9e8680b0f40
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-10-08 22:35:26 +0530

    Time: 51 ms (12.71%), Space: 67.4 MB (10.04%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int binarySearch(int []arr, double val){
+        int low=0, high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=val) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int ans[] = new int[spells.length];
+        int len = potions.length;
+        Arrays.sort(potions);
+        long val=0;
+        for(int i=0;i<spells.length;i++){
+            ans[i]=len-binarySearch(potions,Math.ceil((success*1.0)/spells[i]));
+        }
+        return ans;
+    }
+}
