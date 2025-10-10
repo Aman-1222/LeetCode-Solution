6c28e1a925e5f51a7ff7eeaf3f4b612f3513d6a2
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-10-10 20:38:10 +0530

    Time: 1 ms (99.96%), Space: 47.9 MB (54.11%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,9 +1,10 @@
 class Solution {
     public int sumUtil(int nums[], int goal){
+        if(goal<0) return 0;
         int l=0, r=0, ans=0, sum=0;
         while(r<nums.length){
             sum+=nums[r];
-            while(l<=r && sum>goal){
+            while(sum>goal){
                 sum-=nums[l];
                 l++;
             }
