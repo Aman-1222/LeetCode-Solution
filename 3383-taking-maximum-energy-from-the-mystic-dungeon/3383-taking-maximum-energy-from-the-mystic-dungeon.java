ba6f0f4bc00aca979a1c148eb8b61c75352ed406
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-10-11 18:49:23 +0530

    Time: 2 ms (98.14%), Space: 59.2 MB (92.55%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,13 +1,26 @@
 class Solution {
     public int maximumEnergy(int[] energy, int k) {
+        int len= energy.length;
         int ans=Integer.MIN_VALUE;
-        int len=energy.length-1;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=len;i>=0;i--){
-            map.put(i,energy[i]+map.getOrDefault(i+k,0));
-            ans=Math.max(ans,map.get(i));
+        for(int i=len-k;i<len;i++){
+            int sum=0;
+            for(int j=i;j>=0;j=j-k){
+                sum+=energy[j];
+                ans=Math.max(ans,sum);
+            }
         }
         return ans;
+
+
+
+        // int ans=Integer.MIN_VALUE;
+        // int len=energy.length-1;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=len;i>=0;i--){
+        //     map.put(i,energy[i]+map.getOrDefault(i+k,0));
+        //     ans=Math.max(ans,map.get(i));
+        // }
+        // return ans;
 
 
 
