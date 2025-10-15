2757446ea9dc71c7500d4287890a149e3535c19f
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-10-15 21:17:25 +0530

    Time: 229 ms (51.88%), Space: 173.7 MB (81.58%) - LeetHub

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int maxi=1;
+        int prev=0;
+        int cur=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                cur++;
+            }
+            else
+            {
+                if(prev==0)
+                {
+                    maxi=max(maxi,cur/2);
+                    prev=cur;
+                    cur=1;
+                }
+                else
+                {
+                    int val=min(prev,cur);
+                    maxi=max(maxi,val);
+                    maxi=max(maxi,cur/2);
+                    prev=cur;
+                    cur=1;
+                }
+            }
+        }
+        int val=min(prev,cur);
+        maxi=max(maxi,val);
+        maxi=max(maxi,cur/2);
+        return maxi;
+    }
+};
