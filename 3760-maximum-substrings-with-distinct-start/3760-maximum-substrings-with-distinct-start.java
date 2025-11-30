63d31f3f06cada051309fdbda33c900a666aabfe
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-11-30 17:03:59 +0530

    Time: 32 ms (0%), Space: 47.7 MB (100%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -1,14 +1,8 @@
 class Solution {
     public int maxDistinct(String s) {
         HashSet<Character> set=new HashSet<>();
-        set.add(s.charAt(0));
-        int ans=1;
-        for(int i=1;i<s.length();i++){
-            if(!set.contains(s.charAt(i))){
-                ans++;
-                set.add(s.charAt(i));
-            }
-        }
-        return ans;
+        for(char ch:s.toCharArray())
+                set.add(ch);
+        return set.size();
     }
 }
