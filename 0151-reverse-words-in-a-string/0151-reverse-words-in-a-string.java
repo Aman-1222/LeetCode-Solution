6b4906b13551a50dbe2eeee998998698c20090b0
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2026-01-04 18:56:57 +0530

    Time: 7 ms (66.72%), Space: 46.9 MB (11.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,20 +1,51 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String> st = new Stack<>();
-        String word="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==' '){
-                if(word.length()>0) st.push(word);
-                word="";
-            }
-            else 
-                word=word+s.charAt(i);
-        }
-        if(word.length()>0) st.push(word);
         String ans="";
-        while(!st.empty()){
-            ans=ans+st.pop()+" ";
+        int i=s.length()-1;
+        int end=0;
+        while(i>=0){
+            while(i>=0 && s.charAt(i)==' ') i--;
+            if(i>=0){
+                end=i;
+                while(i>=0 && s.charAt(i)!=' ') i--;
+                ans=ans+s.substring(i+1,end+1)+" ";
+            }
         }
         return ans.substring(0,ans.length()-1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Stack<String> st = new Stack<>();
+        // String word="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)==' '){
+        //         if(word.length()>0) st.push(word);
+        //         word="";
+        //     }
+        //     else 
+        //         word=word+s.charAt(i);
+        // }
+        // if(word.length()>0) st.push(word);
+        // String ans="";
+        // while(!st.empty()){
+        //     ans=ans+st.pop()+" ";
+        // }
+        // return ans.substring(0,ans.length()-1);
     }
 }
