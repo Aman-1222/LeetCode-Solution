90f801a8ef4b766b434c8b0353c1525e8eae47eb
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2026-01-20 11:58:07 +0530

    Time: 1 ms (82.31%), Space: 43.9 MB (30.15%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -23,13 +23,17 @@ class Solution {
         while(!q.isEmpty()){
             int size = q.size();
             List<Integer> lvl = new ArrayList<>();
+            for (int k = 0; k < size; k++) 
+               lvl.add(0); 
             for(int i=0;i<size;i++){
                 TreeNode node = q.poll();
-                lvl.add(node.val);
+                // lvl.add(node.val);
+                int index = (leftToRight)?i:size-i-1;
+                lvl.set(index,node.val);
                 if(node.left!=null) q.add(node.left);
                 if(node.right!=null) q.add(node.right);
             }
-            if(!leftToRight) Collections.reverse(lvl);
+            // if(!leftToRight) Collections.reverse(lvl);
             ans.add(lvl);
             leftToRight=!leftToRight;
         } 
