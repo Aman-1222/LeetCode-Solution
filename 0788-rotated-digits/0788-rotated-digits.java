bcc4a3204cd7952122c07449d6bee269727b4045
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2026-05-02 20:08:18 +0530

    Time: 59 ms (5.43%), Space: 45.5 MB (21.04%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rotatedDigits(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            int rot=0, j=i, cnt=1;
+            while(j>0){
+                int dig=j%10;
+                if(dig==3 || dig==4 || dig==7) break;
+                if(dig==2) rot=5*cnt+rot;
+                else if(dig==5) rot=2*cnt+rot;
+                else if(dig==6) rot=9*cnt+rot;
+                else if(dig==9) rot=6*cnt+rot;
+                else rot=dig*cnt+rot;
+                cnt*=10;
+                j/=10;
+            }
+            if(rot!=i && j==0) {
+                ans++;
+                System.out.println(i);
+            }
+        }
+        return ans;
+    }
+}
